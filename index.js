82fac89e5318fef339804a831ea2f29942e788da
Baka <test@example.com>
2017-01-11 13:50:30 +0300

    added parsing and file counting for master commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let nodegit = require("nodegit"),
+  path = require("path");
+
+let dictionary = {};
+
+nodegit.Repository.open(path.resolve("", ".git"))
+  .then(function (repo) {
+    return repo.getMasterCommit();
+  })
+  .then(function (firstCommitOnMaster) {
+    return firstCommitOnMaster.getTree();
+  })
+  .then(function (tree) {
+    // `walk()` returns an event.
+    let walker = tree.walk();
+    
+    walker.on("entry", function (entry) {
+      if (dictionary[entry.path] == undefined) {
+        dictionary[entry.path] = 0;
+      }
+
+      dictionary[entry.path] += 1;
+    });
+
+    // Don't forget to call `start()`!
+    walker.start();
+  })
+  .then(function () {
+    for (let key in dictionary) {
+      console.log(key + ": " + dictionary[key] + " updates");
+    }
+  })
+.done();
+
