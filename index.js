e72f30504111b9fbb4fbd86bd11940df53c486c9
Baka <test@example.com>
2017-01-13 13:00:39 +0300

    basic tree walker over all commits on current branch
    get ALL files for now, not only changed ones

--- index.js
+++ index.js
@@ -6,30 +6,52 @@ let dictionary = {};
 
 nodegit.Repository.open(path.resolve(repoPath))
   .then(function (repo) {
-    return repo.getMasterCommit();
-  })
-  .then(function (firstCommitOnMaster) {
-    return firstCommitOnMaster.getTree();
-  })
-  .then(function (tree) {
-    // `walk()` returns an event.
-    let walker = tree.walk();
-    
-    walker.on("entry", function (entry) {
-      if (dictionary[entry.path()] == undefined) {
-        dictionary[entry.path()] = 0;
-      }
-
-      dictionary[entry.path()] += 1;
+    return repo.getCurrentBranch()
+    .then(function (branch) {
+      return repo.getBranchCommit(branch.shorthand());
     });
+  })
+  .then(function (branchCommit) {
+    var history = branchCommit.history(),
+        promise = new Promise(function(resolve, reject) {
+          history.on("end", resolve);
+          history.on("error", reject);
+        });
+    
+    history.start();
 
-    // Don't forget to call `start()`!
-    walker.start();
+    return promise;
+  })
+  .then(function (commits) {
+    console.log("commits count: " + commits.length)
+    for (let i = 0; i < commits.length; i++) {
+      var sha = commits[i].sha().substr(0,7),
+          msg = commits[i].message().split('\n')[0];
+      console.log(sha + " " + msg);
+
+      commits[i].getTree().then(function (tree) {
+        // `walk()` returns an event.
+        let walker = tree.walk();
+
+        walker.on("entry", function (entry) {
+          if (dictionary[entry.path()] == undefined) {
+            dictionary[entry.path()] = 0;
+          }
+
+          dictionary[entry.path()] += 1;
+        });
+
+        // Don't forget to call `start()`!
+        walker.start();
+      });
+    }
   })
   .then(function () {
-    for (let key in dictionary) {
-      console.log(key + ": " + dictionary[key] + " updates");
-    }
+    setTimeout(function () {
+      for (let key in dictionary) {
+        console.log(key + ": " + dictionary[key] + " updates");
+      }
+    }, 1000);
   })
 .done();
 
