495667a9ab6cf91a059284d9f25d128dce995b3d
Baka <test@example.com>
2017-01-17 16:29:53 +0300

    started using nodegit-kit

--- index.js
+++ index.js
@@ -1,57 +1,33 @@
-let nodegit = require("nodegit"),
-  path = require("path");
+let git = require("nodegit"),
+    path = require("path"),
+    git_kit = require("nodegit-kit");
 
 let repoPath = ".git";
 let dictionary = {};
 
-nodegit.Repository.open(path.resolve(repoPath))
+git.Repository.open(path.resolve(repoPath))
   .then(function (repo) {
-    return repo.getCurrentBranch()
-    .then(function (branch) {
-      return repo.getBranchCommit(branch.shorthand());
+    // return repo.getBranchCommit("master");
+    return git_kit.log(repo)
+    .then(function (log) {
+      console.log("log:\n");
+      console.log(log);
+      console.log("\n\n");
+
+      return log[0].commit;
+    })
+    .then(function (commit) {
+      return git_kit.diff(repo, commit);
+    })
+    .then(function (diff) {
+      console.log(diff);
     });
   })
-  .then(function (branchCommit) {
-    var history = branchCommit.history(),
-        promise = new Promise(function(resolve, reject) {
-          history.on("end", resolve);
-          history.on("error", reject);
-        });
-    
-    history.start();
-
-    return promise;
-  })
-  .then(function (commits) {
-    console.log("commits count: " + commits.length)
-    for (let i = 0; i < commits.length; i++) {
-      var sha = commits[i].sha().substr(0,7),
-          msg = commits[i].message().split('\n')[0];
-      console.log(sha + " " + msg);
-
-      commits[i].getTree().then(function (tree) {
-        // `walk()` returns an event.
-        let walker = tree.walk();
-
-        walker.on("entry", function (entry) {
-          if (dictionary[entry.path()] == undefined) {
-            dictionary[entry.path()] = 0;
-          }
-
-          dictionary[entry.path()] += 1;
-        });
-
-        // Don't forget to call `start()`!
-        walker.start();
-      });
-    }
-  })
-  .then(function () {
-    setTimeout(function () {
+.done(function () {
+  setTimeout(function () {
       for (let key in dictionary) {
         console.log(key + ": " + dictionary[key] + " updates");
       }
     }, 1000);
-  })
-.done();
+});
 
