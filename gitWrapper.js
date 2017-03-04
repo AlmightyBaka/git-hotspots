a0367412525ed24f58e64d4a08d39795bbb048e3
Almighty Baka <ivanfedulov@almightybaka.com>
2017-03-04 22:26:43 +0300

    added output alignment

--- gitWrapper.js
+++ gitWrapper.js
@@ -1,8 +1,11 @@
 const gitCounter = require("./gitCounter.js");
+const align = require('string-align');
 
 let listPrint = function (repoDir, amount = 10, verbose = false) {
+    console.log(`${align("commits", 10, 'left')}   ${align("filename", 30, 'left')}\n`);
+
     gitCounter.getHotspots(repoDir, (filename, count) => {
-        console.log(`${filename}: ${count}`);
+        console.log(`${align(count, 10, 'left')}   ${align(filename, 30, 'left')}`);
     }, amount, verbose);
 };
 
