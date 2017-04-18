89e2c4f7c88c7cf1ff79b0c9746e971a47ba191d
Almighty Baka <ivanfedulov@almightybaka.com>
2017-04-18 20:57:35 +0300

    added concurrent log getting with raw shell command, limited maximum concurrent processes

--- src/util/gitCounter.js
+++ src/util/gitCounter.js
@@ -1,3 +1,5 @@
+const gochan = require('gochan')
+
 const simpleGit = require('simple-git'),
 getRepoFiles = require('./getRepoFiles.js');
 
@@ -6,33 +8,55 @@ const defRepoDir = ".git";
 let getHotspots = function (repoDir, callback, amount = 10, verbose = false) {
     let getFileLogs = function(files) {
         const git = simpleGit(repoDir);
+        const exec = require('child_process').exec;
+        const tokens = gochan();
         
-        files = files.map(function(file) {
+        for (var i = 0; i < 250; i++) {
+            tokens.put(i)
+        }
+        
+        files = files.map(function(file, index) {
             return new Promise((resolve, reject) => {
-                git.raw(
-                ['log',
-                '--follow',
-                '--oneline',
-                '--',
-                file],
-                (err, result) => {
-                    if (err == null) {
-                        resolve({
-                            file,
-                            count: result.trim().split(/\r?\n/).length
-                        });
-                    }
-                    else {
-                        reject(err);                    
-                    }
-                });
+                tokens.get((err, token) => {
+                    console.log(token, ' started reading file: ', file)
+                    
+                    exec('git --git-dir ' + repoDir + '/.git log --follow --oneline -- ' + file,
+                    (err, stdout, stderr) => {
+                        if (err == null && stderr == "") {
+                            console.log("finished reading file: " + stdout.trim().split(/\r?\n/).length + ' ' + file)
+                            console.log(`index: ${index}, token: ${token}`)
+                            console.log("\n")
+                            
+                            tokens.put(token)
+                            
+                            resolve({
+                                file,
+                                count: stdout.trim().split(/\r?\n/).length
+                            });
+                        }
+                        else {
+                            console.log(err)
+                            console.log(stderr)
+                            
+                            tokens.put(token)                            
+                            
+                            if (err == null) {
+                                reject(stderr)
+                            }
+                            else {
+                                reject(err);                    
+                            }
+                        }
+                    });
+                })
             })
         });
-
+        
+        console.log('total files count: ', files.length)
         return files;
     }
-
-    let resolveFiles = function(filesPromises) {
+    
+    let resolveFiles = async function(filesPromises) {
         return Promise.all(filesPromises)
     }
     
